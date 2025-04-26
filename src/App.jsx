de7c25b8688947fe7725113afbfc5023a7da275d
Krish <potatoduke2005@gmail.com>
2025-04-26 15:50:25 +0530

    fixed build issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./styles/main.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Work from "./components/work";
+import Work from "./components/Work";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 
